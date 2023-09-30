62e7fd40ba70bf5d3eb07c9ce205a36eb8ebc0f1
edmond <edmond.wanyama@student.moringaschool.com>
2023-10-01 02:12:12 +0300

    completed

--- server/app.py
+++ server/app.py
@@ -9,7 +9,7 @@ from models import db, Plant
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = True
+app.json.compact = False
 
 migrate = Migrate(app, db)
 db.init_app(app)
@@ -17,10 +17,52 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+
+    def get(self):
+        plants = [plant.to_dict() for plant in Plant.query.all()]
+        return make_response(jsonify(plants), 200)
+
+    def post(self):
+
+        data = request.get_json()
+
+        new_plant = Plant(
+            name=data['name'],
+            image=data['image'],
+            price=data['price'],
+        )
+
+        db.session.add(new_plant)
+        db.session.commit()
+
+        return make_response(new_plant.to_dict(), 201)
+
+api.add_resource(Plants, '/plants')
 
 class PlantByID(Resource):
-    pass
+
+    def get(self, id):
+        plant = Plant.query.filter_by(id=id).first().to_dict()
+        return make_response(jsonify(plant), 200)
+    
+    def patch(self, id):
+        plant = Plant.query.get(id)
+        
+        data = request.get_json()
+        plant.is_in_stock = data.get('is_in_stock', plant.is_in_stock)
+        db.session.commit()
+
+        return plant.to_dict(), 200
+    def delete(self, id):
+        plant = Plant.query.filter_by(id =id).first()
+
+        db.session.delete(plant)
+        db.session.commit()
+
+        return "", 204
+
+        
+api.add_resource(PlantByID, '/plants/<int:id>')
         
 
 if __name__ == '__main__':
